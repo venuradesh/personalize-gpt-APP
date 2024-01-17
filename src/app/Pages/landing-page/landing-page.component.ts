cdcd28b078779722228df6da5f3b92cfb1e4219b
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2024-01-17 12:41:48 +0530

    changes to the nav ba

--- src/app/Pages/landing-page/landing-page.component.ts
+++ src/app/Pages/landing-page/landing-page.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+///models
+import { navBarModel } from '../../Models/nav_bar_model';
+
+//components
+import { NavBarComponent } from '../../Components/nav-bar/nav-bar.component';
+
 @Component({
   selector: 'pgpt-landing-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavBarComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent {
-
+  navBarElements: navBarModel = {
+    signUp: true,
+    login: true,
+  };
 }
