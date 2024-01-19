5e526de409530a739f33182725bb411a680b9d7a
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2024-01-19 09:35:11 +0530

    registration done with responsiveness

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 //pages
 import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
 import { ModelSelectionComponent } from './Pages/model-selection/model-selection.component';
+import { RegistartionComponent } from './Pages/registartion/registartion.component';
 
 export const routes: Routes = [
   {
@@ -13,6 +14,11 @@ export const routes: Routes = [
   {
     path: 'model-selection',
     component: ModelSelectionComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'registration',
+    component: RegistartionComponent,
     pathMatch: 'full',
   },
 ];
