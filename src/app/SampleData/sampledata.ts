0a757e5555751cdc1f3d075b0dceb397f6fc95d7
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2024-01-21 09:22:48 +0530

    chat section, history section is done with responsiveness

---
+++ src/app/SampleData/sampledata.ts
@@ -0,0 +1,95 @@
+const sampleMessages = [
+  {
+    role: 'user',
+    content: 'What is the capital of Sri Lanka?',
+  },
+  {
+    role: 'bot',
+    content: `Scelerisque rhoncus vivamus montes sapien class facilisi aliquam
+    laoreet mus iaculis leo ut nunc at habitasse vulputate elit tortor
+    quis venenatis praesent fames ridiculus arcu tempor natoque vitae
+    nascetur volutpat tellus urna massa consequat nullam felis litora
+    nec porta non pharetra primis ultrices suspendisse platea vehicula
+    cubilia maecenas ex feugiat`,
+  },
+  {
+    role: 'user',
+    content: 'What is the capital of Sri Lanka?',
+  },
+  {
+    role: 'bot',
+    content: `Scelerisque rhoncus vivamus montes sapien class facilisi aliquam
+    laoreet mus iaculis leo ut nunc at habitasse vulputate elit tortor
+    quis venenatis praesent fames ridiculus arcu tempor natoque vitae
+    nascetur volutpat tellus urna massa consequat nullam felis litora
+    nec porta non pharetra primis ultrices suspendisse platea vehicula
+    cubilia maecenas ex feugiat`,
+  },
+  {
+    role: 'user',
+    content: 'What is the capital of Sri Lanka?',
+  },
+  {
+    role: 'bot',
+    content: `Scelerisque rhoncus vivamus montes sapien class facilisi aliquam
+    laoreet mus iaculis leo ut nunc at habitasse vulputate elit tortor
+    quis venenatis praesent fames ridiculus arcu tempor natoque vitae
+    nascetur volutpat tellus urna massa consequat nullam felis litora
+    nec porta non pharetra primis ultrices suspendisse platea vehicula
+    cubilia maecenas ex feugiat`,
+  },
+  {
+    role: 'user',
+    content: 'What is the capital of Sri Lanka?',
+  },
+  {
+    role: 'bot',
+    content: `Scelerisque rhoncus vivamus montes sapien class facilisi aliquam
+    laoreet mus iaculis leo ut nunc at habitasse vulputate elit tortor
+    quis venenatis praesent fames ridiculus arcu tempor natoque vitae
+    nascetur volutpat tellus urna massa consequat nullam felis litora
+    nec porta non pharetra primis ultrices suspendisse platea vehicula
+    cubilia maecenas ex feugiat`,
+  },
+  {
+    role: 'user',
+    content: 'What is the capital of Sri Lanka?',
+  },
+  {
+    role: 'bot',
+    content: `Scelerisque rhoncus vivamus montes sapien class facilisi aliquam
+    laoreet mus iaculis leo ut nunc at habitasse vulputate elit tortor
+    quis venenatis praesent fames ridiculus arcu tempor natoque vitae
+    nascetur volutpat tellus urna massa consequat nullam felis litora
+    nec porta non pharetra primis ultrices suspendisse platea vehicula
+    cubilia maecenas ex feugiat`,
+  },
+  {
+    role: 'user',
+    content: 'What is the capital of Sri Lanka?',
+  },
+  {
+    role: 'bot',
+    content: `Scelerisque rhoncus vivamus montes sapien class facilisi aliquam
+    laoreet mus iaculis leo ut nunc at habitasse vulputate elit tortor
+    quis venenatis praesent fames ridiculus arcu tempor natoque vitae
+    nascetur volutpat tellus urna massa consequat nullam felis litora
+    nec porta non pharetra primis ultrices suspendisse platea vehicula
+    cubilia maecenas ex feugiat`,
+  },
+  {
+    role: 'user',
+    content: 'What is the capital of Sri Lanka?',
+  },
+  {
+    role: 'bot',
+    content: `Scelerisque rhoncus vivamus montes sapien class facilisi aliquam
+    laoreet mus iaculis leo ut nunc at habitasse vulputate elit tortor
+    quis venenatis praesent fames ridiculus arcu tempor natoque vitae
+    nascetur volutpat tellus urna massa consequat nullam felis litora
+    nec porta non pharetra primis ultrices suspendisse platea vehicula
+    cubilia maecenas ex feugiat`,
+  },
+];
+
+export { sampleMessages };
