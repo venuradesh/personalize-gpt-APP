9b74b19f4633431478a3bd0bb649765cf3d6e3b9
venurawarnasooriya <venura@ashogu.com>
2024-01-31 09:37:03 +0530

    functionality of landing page and model selection page done

--- src/app/Pages/landing-page/landing-page.component.ts
+++ src/app/Pages/landing-page/landing-page.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Router } from "@angular/router";
 
 ///models
-import { navBarModel } from '../../Models/nav_bar_model';
+import { navBarModel } from "../../Models/nav_bar_model";
 
 //components
-import { NavBarComponent } from '../../Components/nav-bar/nav-bar.component';
+import { NavBarComponent } from "../../Components/nav-bar/nav-bar.component";
 
 @Component({
-  selector: 'pgpt-landing-page',
+  selector: "pgpt-landing-page",
   standalone: true,
   imports: [CommonModule, NavBarComponent],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css',
+  templateUrl: "./landing-page.component.html",
+  styleUrl: "./landing-page.component.css",
 })
 export class LandingPageComponent {
   navBarElements: navBarModel = {
     signUp: true,
     login: true,
   };
+
+  constructor(private router: Router) {}
+
+  handleGetStart(): void {
+    this.router.navigate(["/model-selection"]);
+  }
 }
--- src/app/Pages/registartion/registartion.component.ts
+++ src/app/Pages/registartion/registartion.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Location, NgIf } from "@angular/common";
+import { Router } from "@angular/router";
 
 //models
-import { navBarModel } from '../../Models/nav_bar_model';
+import { navBarModel } from "../../Models/nav_bar_model";
 
 //components
-import { NavBarComponent } from '../../Components/nav-bar/nav-bar.component';
-import { InputFieldComponent } from '../../Components/input-field/input-field.component';
+import { NavBarComponent } from "../../Components/nav-bar/nav-bar.component";
+import { InputFieldComponent } from "../../Components/input-field/input-field.component";
 
 @Component({
-  selector: 'pgpt-registartion',
+  selector: "pgpt-registartion",
   standalone: true,
-  imports: [NavBarComponent, InputFieldComponent],
-  templateUrl: './registartion.component.html',
-  styleUrl: './registartion.component.css',
+  imports: [NavBarComponent, InputFieldComponent, NgIf],
+  templateUrl: "./registartion.component.html",
+  styleUrl: "./registartion.component.css",
 })
-export class RegistartionComponent {
+export class RegistartionComponent implements OnInit {
   navBarElements: navBarModel = {
     signUp: false,
     login: true,
   };
+  openAIModelSelected: boolean = false;
+
+  constructor(private router: Router, private location: Location) {}
+
+  ngOnInit(): void {
+    const state: any = this.location.getState();
+    this.openAIModelSelected = state.openAI;
+    console.log(this.openAIModelSelected);
+  }
 }
